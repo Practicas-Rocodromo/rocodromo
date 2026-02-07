68c1f52cb2710d87ceefdb31a100c8bd6855e800
JuanmaJimTen <juanmita.95@gmail.com>
2026-02-07 10:29:02 +0100

    Controlador Usuario y pequeños cambios

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.gbjoanna.rocodromo {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.xml.crypto;
-
-
-    opens com.gbjoanna.rocodromo to javafx.fxml;
-    exports com.gbjoanna.rocodromo;
-}
